8adc46c55784984d9b7fc5d01994600b50098ee3
Enmanuel Lopez <stjacksdev@gmail.com>
2026-09-02 16:18:39 -0600

    andpoint test

--- tests/Feature/MobileProductEndpointTest.php
+++ tests/Feature/MobileProductEndpointTest.php
@@ -472,9 +472,9 @@ class MobileProductEndpointTest extends TestCase
             ->assertUnprocessable()->assertJsonValidationErrors('product');
     }
 
-    public function test_it_sets_and_removes_a_favorite_in_both_tables(): void
+    public function test_android_sets_and_removes_a_favorite_in_both_tables(): void
     {
-        $url = '/api/mobile/v1/catalog/favorites?countryId=1&plataforma=IOS';
+        $url = '/api/mobile/v1/catalog/favorites?countryId=1&plataforma=ANDROID';
 
         $this->postJson($url, [
             'idUser' => 77,
@@ -488,7 +488,7 @@ class MobileProductEndpointTest extends TestCase
         ]);
 
         $this->assertDatabaseHas('stj_favoritos', [
-            'fav_pais' => 1, 'fav_usuario' => 77, 'fav_producto' => 100, 'fav_origen' => 'IOS',
+            'fav_pais' => 1, 'fav_usuario' => 77, 'fav_producto' => 100, 'fav_origen' => 'ANDROID',
         ]);
         $this->assertDatabaseHas('stj_hearts', [
             'hea_pais' => 1, 'hea_usuario' => 77, 'hea_producto' => 100, 'hea_estado' => 'ACTIVO',
